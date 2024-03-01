0f08b5901904804be25b44f5d5cbca568e15d27b
Fatlus <36486145+Savantford@users.noreply.github.com>
2024-03-01 18:26:36 -0300

    Mod data Intensify

--- js/mod-data.js
+++ js/mod-data.js
@@ -1,4 +1,36 @@
 export const ModData = {
-    
+
+    // Ability Strength Mods
+    'Umbral Intensify': {
+        'inArsenal': true,
+        'sharedWith': 'Intensify',
+        'strength': {
+            'percentage': [44, 55, 77]
+        }
+    },
+
+    'Precision Intensify': {
+        'inArsenal': false,
+        'sharedWith': 'Intensify',
+        'strength': {
+            'percentage': [90]
+        }
+    },
+
+    'Intensify': {
+        'inArsenal': true,
+        'sharedWith': 'Intensify',
+        'strength': {
+            'percentage': [30]
+        }
+    },
+
+    'Archon Intensify': {
+        'inArsenal': true,
+        'sharedWith': 'Intensify',
+        'strength': {
+            'percentage': [30, 60]
+        }
+    },
 
 }

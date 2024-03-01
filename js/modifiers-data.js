9b4b367ac679d99cad05426f42add6356f53e4c3
Fatlus <36486145+Savantford@users.noreply.github.com>
2024-03-01 18:47:57 -0300

    Added modifiers data for Strength

---
+++ js/modifiers-data.js
@@ -0,0 +1,146 @@
+export const ModData = {
+
+    // Ability Strength: Arsenal
+    'Umbral Intensify': {
+        'inArsenal': true,
+        'sharedSlot': 'Intensify',
+        'strength': {
+            'percentage': [44, 55, 77]
+        }
+    },
+
+    'Precision Intensify': {
+        'inArsenal': false,
+        'sharedSlot': 'Intensify',
+        'strength': {
+            'percentage': [90]
+        }
+    },
+
+    'Intensify': {
+        'inArsenal': true,
+        'sharedSlot': 'Intensify',
+        'strength': {
+            'percentage': [30]
+        }
+    },
+
+    'Archon Intensify': {
+        'inArsenal': true,
+        'sharedSlot': 'Intensify',
+        'strength': {
+            'percentage': [30, 60]
+        }
+    },
+
+    'Blind Rage': {
+        'inArsenal': true,
+        'strength': {
+            'percentage': [99]
+        },
+        'efficiency': {
+            'percentage': [-55]
+        }
+    },
+
+    'Transient Fortitude': {
+        'inArsenal': true,
+        'strength': {
+            'percentage': [99]
+        },
+        'efficiency': {
+            'percentage': [-55]
+        }
+    },
+
+    'Augur Secrets': {
+        'inArsenal': true,
+        'sharedSlot': '',
+        'strength': {
+            'percentage': [24]
+        }
+    },
+
+    // Ability Strength: Mission Only
+    'Growing Power': {
+        'inArsenal': false,
+        'sharedSlot': 'aura',
+        'strength': {
+            'percentage': [25]
+        }
+    },
+
+    'Power Donation': {
+        'inArsenal': false,
+        'sharedSlot': 'aura',
+        'strength': {
+            'percentage': [-30]
+        }
+    },
+
+    'Energy Conversion': {
+        'inArsenal': false,
+        'sharedSlot': '',
+        'strength': {
+            'percentage': [50]
+        }
+    },
+
+    // Ability Strength: Non-Mod Sources
+
+    'Power Drain': {
+        'inArsenal': false,
+        'source': 'Parazon',
+        'sharedSlot': '',
+        'strength': {
+            'percentage': [50]
+        }
+    },
+
+    'Empower': {
+        'inArsenal': false,
+        'source': 'Helminth',
+        'sharedSlot': 'helminth',
+        'strength': {
+            'percentage': [50]
+        }
+    },
+
+    "Protea's Passive": {
+        'inArsenal': false,
+        'source': 'Protea',
+        'sharedSlot': 'warframe',
+        'strength': {
+            'percentage': [100]
+        }
+    },
+
+    "Invigoration: Strength": {
+        'inArsenal': true,
+        'source': 'Invigoration',
+        'sharedSlot': '',
+        'strength': {
+            'percentage': [200]
+        }
+    },
+
+    "Arbitration Bonus: Strength": {
+        'inArsenal': true,
+        'source': 'Invigoration',
+        'sharedSlot': '',
+        'strength': {
+            'percentage': [200]
+        }
+    },
+
+    // Ability Range
+    'Overextended': {
+        'inArsenal': true,
+        'range': {
+            'percentage': [90]
+        },
+        'strength': {
+            'percentage': [-60]
+        }
+    },
+}
